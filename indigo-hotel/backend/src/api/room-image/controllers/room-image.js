d7e76504958b6ee3c87fb359a43b29f29b5263d6
unknown <umer.rehman996@gmail.com>
2022-03-07 20:24:14 +0500

    All

--- indigo-hotel/backend/src/api/room-image/controllers/room-image.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  room-image controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::room-image.room-image');
--- indigo-hotel/backend/src/api/room-image/routes/room-image.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * room-image router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::room-image.room-image');
